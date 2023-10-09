409d3ddfcc8be13a86d0f6c1659e71cece5ff57e
geeeel <130399415+geeeel@users.noreply.github.com>
2023-10-09 17:53:56 -0300

    Agregamos links que llevan a otros directorios

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
-//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
-
+import CreatePost from './CrearPost';
 import { useState } from 'react';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
 import React from 'react';
 function App() {
   const [count, setCount] = useState(0)
@@ -9,12 +8,19 @@ function App() {
   return (
     <>
       <header style={{backgroundColor:'#101B66', color:'white', padding:'15px'}}>
-        <h1>Bienvenido al Blog</h1>
-        <Link to="/crear-post"classname="boton">Crear Post</Link>
+        
+        <Link to="/CrearPost">CREAR POST</Link>
       </header>
-      
+      <Routes>
+        <Route path="/destino" element={<div>destino</div>} />
+        <Route path="/asd" element={<div>asd</div>} />
+        <Route path="/" element={<div>home</div>} />
+        <Route path="/CrearPost" element={<CreatePost />} />
+
+      </Routes>
     </>
   )
 }
 
 export default App
+  
---
+++ src/CrearPost.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function CreatePost() {
+  return (
+    <div>
+      <h1>CreaDO</h1>
+      <Link to="/destino"></Link>
+    </div>
+  );
+}
+
+export default CreatePost;
